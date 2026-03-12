fccb6ad1a5eb4763d2cf114560ce333f3e30993d
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-03-11 20:46:45 -0600

    Parte 8

--- src/test/java/model/RecursionTest.java
+++ src/test/java/model/RecursionTest.java
@@ -1,7 +1,22 @@
 package model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class RecursionTest {
+
+    @Test
+    void factorialTest() {
+        int n = 5;
+
+        long t1 = System.nanoTime();
+        long result = Recursion.factorial(n);
+        long t2 = System.nanoTime();
+
+        System.out.println("El factorial de " + n + " es " + result + "\nT(n): "+ util.Utility.format(t2-t1) + " ns" );
+
+
+    }
 
 }
