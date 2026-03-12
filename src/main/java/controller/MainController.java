75039966321df9c6975c0c2ecf069e0a3d7c2f1e
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-03-11 20:41:11 -0600

    Parte 7

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,8 +1,17 @@
 package controller;
 
+import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
     @javafx.fxml.FXML
     private Canvas canvasTree;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
---
+++ src/main/java/model/Recursion.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Recursion {
+
+    public static long factorial(int n){
+
+        if (n==0) return 1;
+        return n * factorial(n-1);
+    }
+}
